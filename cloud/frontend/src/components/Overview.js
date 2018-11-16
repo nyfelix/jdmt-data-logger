b11a07fc040b3b76cf81aff2245c51554168b7dd
rkram3r <skill8r@gmail.com>
2018-11-16 15:01:23 +0100

    fix bug.

--- cloud/frontend/src/components/Overview.js
+++ cloud/frontend/src/components/Overview.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as Actions from '../logic/actions';
-import LineChart from './LineChart';
+import LineContainer from './LineContainer';
 
 export default connect(
   state => ({ ...state.reducer }),
   dispatch => bindActionCreators(Actions, dispatch),
-)(({ deviceData }) => (
-  deviceData ? deviceData.map(device => <LineChart key={device.attribute} {...device} />) : null));
+)(
+  ({ deviceData }) => (
+    deviceData ? deviceData.map(device => <LineContainer key={device.attribute} {...device} />)
+      : null),
+);
