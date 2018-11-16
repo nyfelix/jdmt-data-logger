1b809fd50cfd6e87158ace832419925cbc709673
rkram3r <skill8r@gmail.com>
2018-11-16 16:25:46 +0100

    move getDeviceData to detailview.

--- cloud/frontend/src/pages/App.js
+++ cloud/frontend/src/pages/App.js
@@ -1,52 +1,37 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
 import { withRouter } from 'react-router';
+
 import * as Actions from '../logic/actions';
 import Header from '../components/Header';
-
-class Content extends React.Component {
-  componentDidMount() {
-    const {
-      getDeviceData, from, resolution, match: { params: { deviceId } },
-    } = this.props;
-    getDeviceData({ from, resolution, deviceId });
-  }
-
-  render() {
-    const { children, match: { params: { deviceId } } } = this.props;
-
-    return (
-      <div className="wrapper">
-        <div className="d-flex">
-          <Header />
-          <div className="main">
-            <div className="main" key="headertoggle">
-              <nav className="navbar navbar-expand navbar-light bg-white">
-                <span className="sidebar-toggle d-flex mr-2">
-                  <i className="hamburger align-self-center" />
-                </span>
-              </nav>
-            </div>
-            <main className="content">
-              <div className="container-fluid p-0">
-                <div className="clearfix">
-                  <h1 className="h3 mb-3">
-                    {deviceId || 'Dashboard'}
-                  </h1>
-                </div>
-                {children}
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default withRouter(connect(
   state => ({ ...state.reducer }),
   dispatch => bindActionCreators(Actions, dispatch),
-)(Content));
+)(({ children, match: { params: { deviceId } } }) => (
+  <div className="wrapper">
+    <div className="d-flex">
+      <Header />
+      <div className="main">
+        <div className="main" key="headertoggle">
+          <nav className="navbar navbar-expand navbar-light bg-white">
+            <span className="sidebar-toggle d-flex mr-2">
+              <i className="hamburger align-self-center" />
+            </span>
+          </nav>
+        </div>
+        <main className="content">
+          <div className="container-fluid p-0">
+            <div className="clearfix">
+              <h1 className="h3 mb-3">
+                {deviceId || 'Dashboard'}
+              </h1>
+            </div>
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+)));
--- cloud/frontend/src/pages/DetailView.js
+++ cloud/frontend/src/pages/DetailView.js
@@ -1,53 +1,68 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
 import * as Actions from '../logic/actions';
 import LineChart from '../components/LineChart';
 import DeviceControll from '../components/DeviceControll';
 import Map from '../components/Map';
 
-export default connect(
-  state => ({ ...state.reducer }),
-  dispatch => bindActionCreators(Actions, dispatch),
-)(
-  ({ deviceData }) => (
-    <div>
-      <div className="form-row">
-        <div className="col-3 col-md-3 col-lg-3 d-flex">
-          <div className="card flex-fill w-100">
-            <div className="card-body">
-              <DeviceControll />
-            </div>
-          </div>
-        </div>
-        <div className="col-9 col-lg-9 d-flex">
-          <div className="card flex-fill w-100">
-            <div className="card-body">
-              <Map />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        {deviceData && deviceData.map(({ attribute, values }) => (
-          <div key={attribute} className="col-6 col-md-6 col-xl-6 col-lg-6 d-flex order-1 order-xl-2">
+class DetailView extends React.Component {
+  componentDidMount() {
+    const {
+      getDeviceData, from, resolution, match: { params: { deviceId } },
+    } = this.props;
+    getDeviceData({ from, resolution, deviceId });
+  }
+
+  render() {
+    const { deviceData } = this.props;
+
+    return (
+      <div>
+        <div className="form-row">
+          <div className="col-3 col-md-3 col-lg-3 d-flex">
             <div className="card flex-fill w-100">
-              <div className="card-header">
-                <h5 className="card-title mb-0">
-                  {attribute}
-                </h5>
+              <div className="card-body">
+                <DeviceControll />
               </div>
-              <div className="card-body d-flex">
-                <div className="align-self-center w-100">
-                  <div className="py-3">
-                    <LineChart {...{ values }} />
+            </div>
+          </div>
+          <div className="col-9 col-lg-9 d-flex">
+            <div className="card flex-fill w-100">
+              <div className="card-body">
+                <Map />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {deviceData && deviceData.map(({ attribute, values }) => (
+            <div key={attribute} className="col-6 col-md-6 col-xl-6 col-lg-6 d-flex order-1 order-xl-2">
+              <div className="card flex-fill w-100">
+                <div className="card-header">
+                  <h5 className="card-title mb-0">
+                    {attribute}
+                  </h5>
+                </div>
+                <div className="card-body d-flex">
+                  <div className="align-self-center w-100">
+                    <div className="py-3">
+                      <LineChart {...{ values }} />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  ),
-);
+    );
+  }
+}
+
+export default withRouter(connect(
+  state => ({ ...state.reducer }),
+  dispatch => bindActionCreators(Actions, dispatch),
+)(DetailView));
