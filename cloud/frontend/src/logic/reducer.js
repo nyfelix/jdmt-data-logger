7097148d36476f3f8e94126278392b84f87e2d38
rkram3r <skill8r@gmail.com>
2018-11-23 10:30:01 +0100

    simplify.

--- cloud/frontend/src/logic/reducer.js
+++ cloud/frontend/src/logic/reducer.js
@@ -4,23 +4,9 @@ const initialState = {
   resolution: 50,
 };
 export default(oldState = initialState, action) => {
-  switch (action.type) {
-    case 'GET_ALL_DEVICES': {
-      const { devices } = action;
-      console.log(devices);
-      return { ...oldState, devices };
-    }
-    case 'GET_DEVICE_DATA': {
-      const { deviceData } = action;
-
-      return { ...oldState, deviceData };
-    }
-    case 'FROM_CHANGE': {
-      const { from } = action;
-
-      return { ...oldState, from };
-    }
-    default:
-      return oldState;
+  const { type, ...rest } = action;
+  if (type === 'GET_ALL_DEVICES' || type === 'GET_DEVICE_DATA' || type === 'FROM_CHANGE') {
+    return { ...oldState, ...rest };
   }
+  return oldState;
 };
