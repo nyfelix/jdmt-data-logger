9336b3f29afd960e090e849b0135b7feff541054
rkram3r <skill8r@gmail.com>
2019-04-05 10:58:30 +0200

    simplify.

--- training/models/logistic_regression.py
+++ training/models/logistic_regression.py
@@ -15,18 +15,5 @@ Y = numpy.concatenate((numpy.zeros([20]), numpy.ones([59])))
 clf = linear_model.LogisticRegression(C=1e5, solver='lbfgs')
 clf.fit(X, Y)
 
-print(predict(X[0], clf), Y[0])
-print(predict(X[1], clf), Y[1])
-print(predict(X[3], clf), Y[3])
-
-print(predict(X[21], clf), Y[21])
-print(predict(X[22], clf), Y[22])
-print(predict(X[50], clf), Y[50])
-print("extracted...")
-print(predictWithBetas(X[0]), Y[0])
-print(predictWithBetas(X[1]), Y[1])
-print(predictWithBetas(X[3]), Y[3])
-
-print(predictWithBetas(X[21]), Y[21])
-print(predictWithBetas(X[22]), Y[22])
-print(predictWithBetas(X[50]), Y[50])
+for index in range(nofRows):
+    print(predictWithBetas(X[index]), "==", Y[index])
