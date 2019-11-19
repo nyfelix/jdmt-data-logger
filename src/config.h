5ae52a1422c73851c01395f357dde201e040f1df
nyfelix <nyfelix@gmail.com>
2019-11-19 16:38:28 +0100

    Update config.h
    Added final coordinates

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@ This file is for setting up the embedded device mode and tune the settings. The 
  ***********************************************/
 
 /*************************** *******PRECOMPILE SETTINGS *******************************/
-#define DEBUG 
+//#define DEBUG 
 //#define DEEPSLEEP
 //#define NO_EMERGENCY// programm will not change to emergency state even the camermodul was removed
 //#define PRINT_PICTURE // Prints the picture in a uint8_t array in the console
@@ -27,9 +27,9 @@ This file is for setting up the embedded device mode and tune the settings. The 
 
 
 #define DEVICE_TYPE 1 //0== ZOLL AED 3, 1== CU_S, 2== ZOLL AED PLUS
-#define DEVICE_NR 3  // Handling the diffrent devices
+#define DEVICE_NR 2  // Handling the diffrent devices
 #define DATARATE SF12BW125
-#define SENDING_PERIOD 30
+#define SENDING_PERIOD 30 // Seconds
 
 
 
@@ -114,7 +114,7 @@ This file is for setting up the embedded device mode and tune the settings. The 
 #endif
 
 
-
+// Standort Socar Rapperswil (indoor)
 #if DEVICE_NR==1
 
   #define NWKSKEY { 0xEC, 0x33, 0x12, 0x52, 0x0B, 0x26, 0xC2, 0x60, 0x89, 0xB5, 0x1D, 0x20, 0x2D, 0x11, 0x96, 0x4C }
@@ -130,17 +130,19 @@ This file is for setting up the embedded device mode and tune the settings. The 
 
 #endif
 
+// Standort Socar Pfäffikon (outdoor)
 #if DEVICE_NR==2
                     
   #define NWKSKEY { 0xC6, 0x1B, 0xC7, 0x68, 0x49, 0x9E, 0xF2, 0x8E, 0x63, 0x26, 0x0D, 0x34, 0xEC, 0x3A, 0x46, 0xE1 }
   #define APPSKEY { 0xDC, 0xB1, 0x9A, 0xDB, 0xA4, 0x90, 0x98, 0xD8, 0x14, 0x08, 0x9B, 0x77, 0x72, 0xA3, 0xF0, 0xFD }
   #define DEVADDR { 0x26, 0x01, 0x17, 0x02 }
 
-  #define Device_Position_latitude  47.533163f
-  #define Device_Position_longitude  8.731643f
+  #define Device_Position_latitude  47.365292f
+  #define Device_Position_longitude  8.785678f
 
 #endif
 
+// Standort Socar Seuzach (outdoor)
 #if DEVICE_NR==3
                     
   #define NWKSKEY { 0xB0, 0xB4, 0x60, 0x6C, 0x0A, 0x1C, 0x1D, 0xC1, 0xD4, 0x13, 0x4B, 0x15, 0xB0, 0x3A, 0x6F, 0x62}
